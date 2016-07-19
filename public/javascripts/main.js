28b57c1873c5cbec5a0255697b60bdcb48d6c874
R.A. Lucas <richard@richardalucas.com>
2016-07-18 21:45:37 -0600

    updating

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,53 @@
 $(function() {
 
-  function buildHtml(tests) {
-    var compiled = _.template('<% _.forEach(tests, function(test, k) { %>' +
-      '<div class="test">' +
-        '<h4 class="desc"><%= test.description %></h4>' +
-        '<p class="trace">Traceback: <div class="trace-out">' +
-           '<% _.forEach(test.traceback, function(t) { %>' +
-           '<p><code><%= t %></code></p>' + 
-            '<% }) %></div></p>' +
-        '<h5>Results:</h5>' +
-        '<ul>' +
-        '<% _.forEach(test.output, function(output, key) { %>' +
-          '<li><span class="key"><%= _.startCase(key) %></span> : ' +
-            '<div class="results-out"><% _.forEach(output, function(o) { %>' +
-               '<p><code><%= o %></code></p>' + 
-            '<% }) %><div></li>' +
-        '<% }); %>' +
-        '</ul>' +
-      '</div>' +
+  function buildHtml(json) {
+    var compiled = _.template(
+      '<h3>Title: <%= json.name %></h3>' +
+      '<p class="date">Date: <%= new Date(json.date) %></p>' +
+      '<h5>Character count: <%= json.charCount %></h5>' +
+      '<p>File Stats:</p><ul>' +
+      '<% _.forEach(json.stats, function(s, k) { %> ' +
+        '<li><%= k %> : <span class="stat"><code><%= s %></code></span></li>' +
+      '<% }) %></ul>' +
+      '<% _.forEach(json.tests, function(test, k) { %>' +
+        '<div class="test <% if (test.output.passfail == \'failed\') { %> fail <% } %>">' +
+          '<h4 class="desc"><%= test.description %></h4>' +
+          '<p class="trace">Traceback: <div class="trace-out">' +
+             '<% _.forEach(test.traceback, function(t) { %>' +
+             '<p><code><%= t %></code></p>' + 
+              '<% }) %></div></p>' +
+          '<h5>Results:</h5>' +
+          '<ul>' +
+          '<% _.forEach(test.output, function(output, key) { %>' +
+            '<li><span class="key"><%= _.startCase(key) %></span> : ' +
+              '<div class="results-out"><% _.forEach(output, function(o) { %>' +
+                 '<p><code><%= o %></code></p>' + 
+              '<% }) %><div></li>' +
+          '<% }); %>' +
+          '</ul>' +
+        '</div>' +
      '<% }); %>');
-     return compiled({tests: tests});
+     return compiled({json: json});
   }
 
   $('select').on('change', function() {
     var file = $(this).val();
-    var requestUrl = '/json?file=' + file;
+    console.log('rq', requestUrl);
+    $.get(requestUrl, function(json) {
+      console.log(json);
+      var fails = _.find(json.tests.output, function(o, k) {
+        return o == 'failure';
+      }).length;
+      var passes = json.tests.length - fails;
+      _.extend(json, {fails: fails, passes: passes});
+      var html = buildHtml(json);
+      $('.output').append(html);
+    });
+  });
+
+  $('.show').on('click', function() {
+    var file = $('select').val();
+    var requestUrl = '/api/json' + window.location.pathname + '?file=' + file;
     $.get(requestUrl, function(json) {
       console.log(json);
       var html = buildHtml(json);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var fs = require('fs');
 var _ = require('lodash');
 
-const BASE_DIR = '/Users/richardlucas/gatech_class_material/cs8803/pr1/';
+const BASE_DIR = '/Users/richardlucas/gatech_class_material/cs8803/';
 
 function getFiles(dir, type) {
   var typeRe = new RegExp(type);
@@ -17,36 +17,80 @@ function readFile(file, subdir) {
   return fs.readFileSync(BASE_DIR + subdir + '/' + file, {encoding: 'utf-8'});
 }
 
-var files = getFiles('gflib', 'result').map(function(file) {
-  var json = JSON.parse(readFile(file, 'gflib'));
-  var tests = _.forEach(json.tests, function(j) {
-    j.traceback = j.traceback.split('\n');
-    _.forEach(j.output, function(out, k) {
-      j.output[k] =  _.toString(out).split('\n');
-    }); 
+function getResults(dir) {
+  return getFiles(dir, 'result').map(function(file) {
+    var read = readFile(file, dir);
+    var json = JSON.parse(read);
+    var charCount = read.length;
+    var tests = _.forEach(json.tests, function(j) {
+      j.traceback = j.traceback.split('\n');
+      _.forEach(j.output, function(out, k) {
+        j.output[k] =  _.toString(out).split('\n');
+      }); 
+    });
+    var fileLoc = BASE_DIR + dir + '/' + file;
+    var o = {
+      name: file,
+      date: parseDate(file), 
+      tests: tests,
+      charCount: charCount,
+      stats: fs.statSync(fileLoc) 
+    };
+    return o;
+  }).sort(function(a, b) {
+    return b.date - a.date;
   });
-  var o = {
-    name: file,
-    json: tests 
-  };
-  return o;
+}
+
+function parseDate(str) {
+  var timedateSplit = str.split('.json').shift().split('-').filter((ea) => { return !isNaN(Number(ea)); });
+  var time = timedateSplit.slice(3).join(':');
+  var date = timedateSplit.slice(1, 3).join('-');
+  var datetime = date + ' ' + time;
+  return new Date(datetime);
+}
+
+router.get('/', function(req, res) {
+  res.render('index', { 
+    title: 'Autograder Viewer'
+  });
 });
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  console.log(files.length);
-  res.render('index', { 
+router.get('/:dir', function(req, res) {
+  var dir = req.params.dir || 'gflib';
+  res.render('dir', { 
     title: 'Autograder Viewer',
-    files: files 
+    files: getResults(dir) 
   });
 });
 
-router.get('/json', function(req, res) {
-  var fileName = req.query.file;
-  var file = files.find(function(file) {
+router.get('/:dir/:filename', function(req, res) {
+  var dir = req.params.dir; 
+  var fileName = req.params.file;
+  if (!dir || !fileName) {
+    return res.status(404).send('Not Found.');
+  }
+  var file = getResults(dir).find(function(file) {
     return file.name == fileName;
   });
-  res.json(file.json);
+  res.render('file', {
+    file: file
+  });
+});
+
+router.get('/api/json/:dir', function(req, res) {
+  var dir = req.params.dir || 'gflib';
+  var fileName = req.query.file;
+  var file = getResults(dir).find(function(file) {
+    return file.name == fileName;
+  });
+  res.json(file);
+});
+
+router.get('/reset', function(req, res) {
+  res.send('Reset: ' + new Date());
+  process.exit(1);
 });
 
 module.exports = router;
